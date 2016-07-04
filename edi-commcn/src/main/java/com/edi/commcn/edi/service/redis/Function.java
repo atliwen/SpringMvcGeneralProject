2f12893938fd7f52e14177c805f2faafcee7339b
atliwen <499014099@qq.com>
2016-07-04 10:06:50 +0800

    更新

--- edi-commcn/src/main/java/com/edi/commcn/edi/service/redis/Function.java
+++ edi-commcn/src/main/java/com/edi/commcn/edi/service/redis/Function.java
@@ -3,7 +3,7 @@ package com.edi.commcn.edi.service.redis;
 /**
  * 回调方法
 * <p>Title: Function </p>
-* <p>@Description: TODO </p>
+* <p>@Description: </p>
 * <p>Company:  </p>
 * @author 李文
 * @date   2016年6月21日 上午10:03:49
