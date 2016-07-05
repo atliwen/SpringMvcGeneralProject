0a67f736bce7049d79df2e7b806b56f43cbab7d2
atliwen <499014099@qq.com>
2016-07-05 15:55:12 +0800

    更新

--- edi-manage/edi-manage-service/src/main/java/com/edi/manage/service/EdiUserService.java
+++ edi-manage/edi-manage-service/src/main/java/com/edi/manage/service/EdiUserService.java
@@ -3,6 +3,7 @@ package com.edi.manage.service;
 import org.springframework.stereotype.Service;
 
 import com.edi.manage.pojo.EdiUser;
+import com.edi.manage.service.base.BaseService;
 
 @Service
 public class EdiUserService extends BaseService<EdiUser>
