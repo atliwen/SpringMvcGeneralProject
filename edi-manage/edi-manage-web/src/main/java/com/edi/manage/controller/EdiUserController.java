e53ac836ebf03dfcca5e30a2b8248eb57719266c
atliwen <499014099@qq.com>
2016-07-05 14:17:43 +0800

    更新

--- edi-manage/edi-manage-web/src/main/java/com/edi/manage/controller/EdiUserController.java
+++ edi-manage/edi-manage-web/src/main/java/com/edi/manage/controller/EdiUserController.java
@@ -1,5 +1,7 @@
 package com.edi.manage.controller;
 
+import java.util.Date;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,16 @@ public class EdiUserController extends BaseController<EdiUserService, EdiUser>
 	public ResponseEntity<EdiUser> queryMByid(@PathVariable("id") Long id)
 	{
 		EdiUser user = new EdiUser();
+		user.setClientflag("111");
+		user.setDataflag("111");
+		user.setDatatype("111");
+		user.setId(111L);
+		user.setName("111");
+		user.setOrdertype(1);
+		user.setUpdatetime(new Date());
+		user.setRecordtime(new Date());
+		user.setVerifyseed("1");
+		user.setXid(11);
 		user.setIp("100000");
 		return ResponseEntity.ok(user);
 	}
